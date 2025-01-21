6d6963eb41ad7f2cb95078a93f4f6fcaaaf6408e
David Xu <rav4@berkeley.edu>
2025-01-20 19:56:33 -0800

    Pushing privimitive retreat... tune later

---
+++ java/src/v8/SplasherMicro.java
@@ -0,0 +1,179 @@
+package v8;
+
+import battlecode.common.*;
+
+public class SplasherMicro {
+
+    final int INF = 1000000;
+    boolean shouldPlaySafe = false;
+    static int myVisionRange = GameConstants.VISION_RADIUS_SQUARED;
+    static final int RANGE = UnitType.SOLDIER.actionRadiusSquared;
+
+    static final Direction[] dirs = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+            Direction.CENTER
+    };
+
+    final static int MAX_MICRO_BYTECODE_REMAINING = 2000;
+
+    static RobotController rc;
+
+    SplasherMicro(RobotController rc){
+        this.rc = rc;
+    }
+
+    static boolean canAttack;
+
+    static boolean isTower = false;
+    static MapLocation enemyTower;
+    boolean doMicro(MapLocation target, boolean tower){
+        try {
+            if (!rc.isMovementReady()) return false;
+            isTower = tower;
+            shouldPlaySafe = false;
+            RobotInfo[] units = rc.senseNearbyRobots(myVisionRange, rc.getTeam().opponent());
+
+            canAttack = rc.isActionReady();
+            enemyTower = target;
+
+            MicroInfo[] microInfo = new MicroInfo[9];
+            for (int i = 0; i < 9; ++i) microInfo[i] = new MicroInfo(dirs[i]);
+
+            for (RobotInfo unit : units) {
+                if (Clock.getBytecodesLeft() < MAX_MICRO_BYTECODE_REMAINING) break;
+
+                microInfo[0].updateEnemy(unit);
+                microInfo[1].updateEnemy(unit);
+                microInfo[2].updateEnemy(unit);
+                microInfo[3].updateEnemy(unit);
+                microInfo[4].updateEnemy(unit);
+                microInfo[5].updateEnemy(unit);
+                microInfo[6].updateEnemy(unit);
+                microInfo[7].updateEnemy(unit);
+                microInfo[8].updateEnemy(unit);
+            }
+
+            units = rc.senseNearbyRobots(myVisionRange, rc.getTeam());
+            for (RobotInfo unit : units) {
+                if (Clock.getBytecodesLeft() < MAX_MICRO_BYTECODE_REMAINING) break;
+
+                microInfo[0].updateAlly(unit);
+                microInfo[1].updateAlly(unit);
+                microInfo[2].updateAlly(unit);
+                microInfo[3].updateAlly(unit);
+                microInfo[4].updateAlly(unit);
+                microInfo[5].updateAlly(unit);
+                microInfo[6].updateAlly(unit);
+                microInfo[7].updateAlly(unit);
+                microInfo[8].updateAlly(unit);
+            }
+
+            MicroInfo bestMicro = microInfo[8];
+            for (int i = 0; i < 8; ++i) {
+                if (microInfo[i].isBetter(bestMicro)) bestMicro = microInfo[i];
+            }
+
+            if (bestMicro.dir == Direction.CENTER) return false;
+
+            if (rc.canMove(bestMicro.dir)) {
+                rc.move(bestMicro.dir);
+                return true;
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+
+    class MicroInfo {
+        Direction dir;
+        MapLocation location;
+        int numMoppers = 0;
+        boolean canMove = true;
+        int penalty = 0;
+        PaintType pt;
+        boolean inTowerRange = false;
+        boolean canHitTower = false;
+        int distToTower = INF;
+        int pti = 0;
+        public MicroInfo(Direction dir) throws GameActionException {
+            this.dir = dir;
+            this.location = rc.getLocation().add(dir);
+
+            if (dir != Direction.CENTER && !rc.canMove(dir)) {
+                canMove = false;
+                return;
+            }
+            MapInfo mi = rc.senseMapInfo(this.location);
+            if (isTower && enemyTower.isWithinDistanceSquared(location, RANGE)) inTowerRange = true;
+            distToTower = enemyTower.distanceSquaredTo(location);
+            pt = mi.getPaint();
+            if (pt.isEnemy()) {
+                penalty = 4;
+                pti = 2;
+            }
+            if (pt == PaintType.EMPTY) {
+                penalty = 2;
+                pti = 1;
+            }
+
+        }
+
+        void updateEnemy(RobotInfo unit){
+            if (!canMove) return;
+            if (unit.getType() == UnitType.MOPPER) {
+                if (unit.getLocation().isWithinDistanceSquared(location, 2)) numMoppers++;
+            }
+        }
+
+        void updateAlly(RobotInfo unit){
+            if (!canMove) return;
+            if(unit.getLocation().isWithinDistanceSquared(location, 2))
+                penalty += pt.isEnemy() ? 2 : 1;
+        }
+
+        // What info do we need to collect?
+        // Prioritize safety
+        // Then for the square that allows you to get good "worth"
+        // - lots of bytecode... we can approx via "num enemy paint seen"
+        // Then minimize penalties, etc.
+        // How far are we from target?
+        // type of paint
+        // how many enemy moppers
+        // paint penalty from allies
+        //equal => true
+        boolean isBetter(MicroInfo M){
+            if(canMove && !M.canMove) return true;
+            if(!canMove && M.canMove) return false;
+
+            if (inTowerRange && !M.inTowerRange) return false;
+            if (!inTowerRange && M.inTowerRange) return true;
+
+            //Aggressive...
+            if (distToTower < M.distToTower) return true;
+            if (M.distToTower < distToTower) return false;
+
+            if (numMoppers < M.numMoppers) return true;
+            if (M.numMoppers < numMoppers) return false;
+
+            if (penalty < M.penalty) return true;
+            if (M.penalty < penalty) return false;
+
+            if (pti < M.pti) return true;
+            if (M.pti < pti) return false;
+
+            return true;
+        }
+    }
+
+}
---
+++ java/src/v8/fast/FastIntSet.java
@@ -0,0 +1,66 @@
+package v8.fast;
+
+public class FastIntSet {
+    public StringBuilder keys;
+    public int size;
+
+    public FastIntSet() {
+        keys = new StringBuilder();
+        size = 0;
+    }
+
+    public static int c2i(char a, char b) {
+        return (a << 16) | b;
+    }
+    public static String i2c(int value) {
+        return (char) ((value >> 16) & 0xFFFF ) + "" // Higher 16 bits
+             + (char) (value & 0xFFFF );        // Lower 16 bits
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(int i) {
+        String key = i2c(i);
+        if (keys.indexOf(key) < 0) {
+            keys.append(key);
+            size++;
+        }
+    }
+
+    public void remove(int i) {
+        String key = i2c(i);
+        int index;
+        if ((index = keys.indexOf(key)) >= 0) {
+            keys.delete(index, index + 2);
+            size--;
+        }
+    }
+
+    public boolean contains(int i) {
+        return keys.indexOf(i2c(i)) >= 0;
+    }
+
+    public void clear() {
+        size = 0;
+        keys = new StringBuilder();
+    }
+
+    public int pop() {
+        if (size == 0)
+            return -1;
+        int i = peek();
+        remove(i);
+        return i;
+    }
+    public int peek() {
+        if (size == 0)
+            return -1;
+        return c2i(keys.charAt(0), keys.charAt(1));
+    }
+
+    public int at(int i) {
+        return c2i(keys.charAt(i * 2), keys.charAt(i * 2 + 1));
+    }
+}
